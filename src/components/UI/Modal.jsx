0e20bf4c04172555418dac394a6aa834ab524b29
Benecia <crastabenecia9@gmail.com>
2023-11-18 11:41:40 +0530

    responsive modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -5,8 +5,8 @@ const Modal = ({ activeID, setShowModal }) => {
   const portfolio = portfolios.find((portfolio) => portfolio.id === activeID);
 
   return (
-    <div className="w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-40">
-      <div className="w-11/12 md:max-w-[600px] md:w-full absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform-translate-x-1/2 -translate-y-1/2 p-5">
+    <div className="overflow-auto w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-40">
+      <div className=" w-11/12 md:max-w-[600px] md:w-full absolute sm:top-[75%] sm:left-[27%] left-4 top-[55%] z-20 bg-white rounded-[8px] transform-translate-x-1/2 -translate-y-1/2 p-5 ">
         <div>
           <figure>
             <img className="rounded-[8px]" src={portfolio.imgUrl} alt="" />

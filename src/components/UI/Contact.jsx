e6f95a7b32095c0cfdb17fdfef00ca582e11c7ab
Benecia <crastabenecia9@gmail.com>
2023-11-23 17:01:31 +0530

    toaster and emailjs implemented

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qmz5d28",
+        "template_8udbg2m",
+        form.current,
+        "WrNe9Fjw_7xV2WxJH"
+      )
+      .then(
+        (result) => {
+          toast.success("Message sent!", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        },
+        (error) => {
+          toast.error("Oh-uh! Some error occured", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      );
+  };
+
   return (
-    <section id="contact" className="pb-16">
+    <section id="contact" className="pb-16 sm:pt-8 pt-2">
       <div className="container">
-        <h2 className="text-headingColor font-[700] text-[2.5rem] mb-8">
+        <h2 className="text-headingColor font-[700] text-[2.5rem] mb-4">
           Get in touch
         </h2>
         <div className="md:flex justify-between items-center">
@@ -20,24 +63,29 @@ const Contact = () => {
           </div>
 
           <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 lg:px-8 py-8">
-            <form className="w-full">
-              <div className="mb-5">
+            <form className="w-full" ref={form} onSubmit={sendEmail}>
+              {/* ref={form} onSubmit={sendEmail} */}
+              <div className="mb-5 mx-4 sm:mx-2">
                 <input
                   type="text"
                   placeholder="Enter your name"
                   className="w-full p-3 focus:outline-none rounded-[5px]"
+                  name="user_name"
+                  required
                 />
               </div>
 
-              <div className="mb-5">
+              <div className="mb-5  mx-4 sm:mx-2">
                 <input
                   type="email"
                   placeholder="Enter your email"
                   className="w-full p-3 focus:outline-none rounded-[5px]"
+                  name="user_email"
+                  required
                 />
               </div>
 
-              <div className="mb-5">
+              <div className="mb-5  mx-4 sm:mx-2">
                 <input
                   type="text"
                   placeholder="Subject"
@@ -45,18 +93,36 @@ const Contact = () => {
                 />
               </div>
 
-              <div className="mb-5">
+              <div className="mb-5  mx-4 sm:mx-2">
                 <textarea
                   type="text"
                   rows={3}
                   placeholder="Write your message"
                   className="w-full p-3 focus:outline-none rounded-[5px]"
+                  name="message"
+                  required
                 />
               </div>
-
-              <button className="w-full p-3 focus:outline-none rounded-[5px] bg-smallTextColor text-white hover:bg-headingColor text-center ease-linear duration-150">
-                Send message
-              </button>
+              <div className="flex justify-center">
+                <input
+                  className="w-[91%] sm:w-[97%] p-3 mx-auto focus:outline-none rounded-[5px] bg-smallTextColor text-white hover:bg-headingColor text-center ease-linear duration-150"
+                  type="submit"
+                  value="Send message"
+                  required
+                />
+                <ToastContainer
+                  position="top-right"
+                  autoClose={3000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="light"
+                />
+              </div>
             </form>
           </div>
         </div>
